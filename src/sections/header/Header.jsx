9e6c29f60e01f45b877ad657566d23f6ac2210ff
Mohammad Taquie <alitaquie51@gmail.com>
2024-03-21 15:21:09 -0700

    navbar and header finished

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,9 +1,30 @@
+import HeaderImage from '../../assets/header.jpg'
+import data from './data'
 import './header.css'
 
 const Header = () => {
   return (
-    <header id="header"><h2>Header</h2></header>
+    <header  id="header">
+      <div className='container header_containter'>
+        <div className='header_profile'>
+          <img src ={HeaderImage} alt="Header Portrait"/>
+          </div>
+          <h3>Ali Taquie</h3>
+          <p>
+            blah blah blah please give me
+          </p>
+          <div className='header_cta'>
+            <a href = "#contact" className='btn_primary'>Lets Talk</a>
+            <a href = "#portfolio" className='btn_light'>My work</a>
+          </div>
+          <div className='header_socials'>
+            {
+              data.map(item => <a key ={item.id} href ={item.link} target = "_blank" rel = "noopener noreferrer">{item.icon}</a>)
+            }
+          </div>
+        </div>     
+    </header>
   )
 }
 
-export default Header
+export default Header 
---
+++ src/sections/header/data.js
@@ -0,0 +1,9 @@
+import { AiFillLinkedin } from "react-icons/ai";
+import {AiFillGithub} from 'react-icons/ai'
+
+const data = [
+    {id: 1, link: 'https://www.linkedin.com/in/ali-taquie-82481b17a/', icon:  <AiFillLinkedin />},
+    {id: 2, link: 'https://github.com/alitaquie?tab=repositories', icon: <AiFillGithub/>}
+]
+
+export default data
