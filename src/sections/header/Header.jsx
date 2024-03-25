4019530adf0080fc267885b0ebfdc391b73fa835
Mohammad Taquie <alitaquie51@gmail.com>
2024-03-24 17:10:26 -0700

    about section complete, css finished

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -4,27 +4,27 @@ import './header.css'
 
 const Header = () => {
   return (
-    <header  id="header">
-      <div className='container header_containter'>
-        <div className='header_profile'>
+    <header id="header">
+      <div className="container header__container">
+        <div className="header__profile">
           <img src ={HeaderImage} alt="Header Portrait"/>
-          </div>
-          <h3>Ali Taquie</h3>
-          <p>
-            blah blah blah please give me
-          </p>
-          <div className='header_cta'>
-            <a href = "#contact" className='btn_primary'>Lets Talk</a>
-            <a href = "#portfolio" className='btn_light'>My work</a>
-          </div>
-          <div className='header_socials'>
-            {
-              data.map(item => <a key ={item.id} href ={item.link} target = "_blank" rel = "noopener noreferrer">{item.icon}</a>)
-            }
-          </div>
-        </div>     
+        </div>
+        <h3>Ali Taquie</h3>
+        <p>
+        someone please give me an internship
+        </p>
+        <div className="header__cta">
+          <a href="#contact" className='btn primary'>Let's Talk</a>
+          <a href="#portfolio" className='btn light'>My Work</a>
+        </div>
+        <div className="header__socials">
+          {      data.map(item => <a key={item.id} href={item.link} target="_blank" rel="noopener noreferrer">{item.icon}</a>)
+      
+          }
+        </div>
+      </div>
     </header>
   )
 }
 
-export default Header 
+export default Header
